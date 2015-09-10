36c59eda6acc5ed126c251c09e819ed997b54070
Brad Fisher <bradallenfisher@gmail.com>
2015-09-10 16:05:59 -0400

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -10,3 +10,7 @@ echo PATH='$HOME/.composer/vendor/bin:$PATH' >> $HOME/.bashrc
 echo 'alias install="/usr/local/bin/scripts/si.sh"' >> $HOME/.bashrc
 echo export PATH >> $HOME/.bashrc
 source $HOME/.bashrc
+
+drush dl drush_recipes -y
+drush dl drush_cleanup -y
+drush dl registry_rebuild -y

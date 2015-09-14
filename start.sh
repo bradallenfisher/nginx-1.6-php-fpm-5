42d2e6b94d07cf74525cb9080860736f1cabcaa7
Brad Fisher <bradallenfisher@gmail.com>
2015-09-14 14:29:09 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -58,13 +58,13 @@ git clone https://github.com/perusio/drupal-with-nginx.git /etc/nginx
 cd /etc/nginx
 git checkout D7
 
-cat nginx.conf.txt > /etc/nginx/nginx.conf
-cat php-fpm.conf.txt > /etc/php-fpm.conf
-cat www.conf.txt > /etc/php-fpm.d/www.conf
-cat drupal.conf.txt > /etc/nginx/apps/drupal/drupal.conf
-cat example.conf.text > /etc/nginx/sites-available/test.com.conf
-cat varnish.txt > /etc/sysconfig/varnish
-cat default.vcl.txt > /etc/varnish/default.vcl
+cat $HOME/nginx-1.6-php-fpm-5/nginx.conf.txt > /etc/nginx/nginx.conf
+cat $HOME/nginx-1.6-php-fpm-5/php-fpm.conf.txt > /etc/php-fpm.conf
+cat $HOME/nginx-1.6-php-fpm-5/www.conf.txt > /etc/php-fpm.d/www.conf
+cat $HOME/nginx-1.6-php-fpm-5/drupal.conf.txt > /etc/nginx/apps/drupal/drupal.conf
+cat $HOME/nginx-1.6-php-fpm-5/example.conf.text > /etc/nginx/sites-available/test.com.conf
+cat $HOME/nginx-1.6-php-fpm-5/varnish.txt > /etc/sysconfig/varnish
+cat $HOME/nginx-1.6-php-fpm-5/default.vcl.txt > /etc/varnish/default.vcl
 
 
 mkdir -p /etc/nginx/sites-enabled
